7fcbb5bf60ee2b19695f4d255d19484d88dce405
kanta <21238358@edu.cc.saga-u.ac.jp>
2025-09-02 19:38:55 +0900

    クエリパラメータとパスパラメータ違い
    re.httpがapiのテストができるファイル

--- app/main.py
+++ app/main.py
@@ -4,12 +4,48 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"にょす": "World"}
+async def read_root():
+    return {"message": "Hello World GET"}
 
+@app.post("/")
+async def create_root():
+    return {"message": "Hello World POST"}
 
+@app.put("/")
+async def update_root():
+    return {"message": "Hello World PUT"}
+
+@app.patch("/")
+async def partial_update_root():
+    return {"message": "Hello World PATCH"}
+
+@app.delete("/")
+async def delete_root():
+    return {"message": "Hello World DELETE"}
+
+#パスパラメータを用いたAPI
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+#クエリパラメータを用いたAPI
+@app.get("/items/")
+async def read_items(q: Optional[str] = None):
+    if q:
+        return {"q": q}
+    return {"items": ["item1", "item2", "item3"]}
+
+#演習課題１(パスパラメータ)
+@app.get("/hello/{user_name}")
+async def read_user(user_name: str):
+    return {"message": f"Hello, {user_name}"}
+
+#演習課題２(クエリパラメータ)
+@app.get("/info/")
+async def get_items(item: Optional[str]=None):
+    available_items = ["apple", "banana","orange"]
+    if item:
+        return {"requested_item": item}
+    return{"available_items" : available_items}
+
